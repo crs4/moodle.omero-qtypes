5ba272980095c484accd8449b3b51a813adedc62
Marco Enrico Piras <kikkomep@crs4.it>
2015-06-16 15:41:13 +0200

    fixes unmodified template files with the actual plugin name

--- edit_omeromultichoice_form.php
+++ edit_omeromultichoice_form.php
@@ -18,7 +18,7 @@
  * Defines the editing form for the omeromultichoice question type.
  *
  * @package    qtype
- * @subpackage YOURQTYPENAME
+ * @subpackage omeromultichoice
  * @copyright  2015 CRS4
 
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
--- questiontype.php
+++ questiontype.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->libdir . '/questionlib.php');
 require_once($CFG->dirroot . '/question/engine/lib.php');
-require_once($CFG->dirroot . '/question/type/YOURQTYPENAME/question.php');
+require_once($CFG->dirroot . '/question/type/omeromultichoice/question.php');
 
 
 /**
