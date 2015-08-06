83926d0f1a71a09ff6322b0ce7686f3fa4480f49
Marco Enrico Piras <kikkomep@crs4.it>
2015-08-06 20:02:14 +0200

    added basic support for event handling produced by the wrapped OmeroWebViewer

--- omero_multichoice_helper.js
+++ omero_multichoice_helper.js
@@ -7,8 +7,43 @@ M.omero_multichoice_helper = {};
  * @param module_name
  * @param options
  */
-M.omero_multichoice_helper.init = function(module_name, options){
+M.omero_multichoice_helper.init = function (module_name, options) {
     M.omero_multichoice_helper.module_name = module_name;
+
+    document.addEventListener("frameLoaded", function (e) {
+        M.omero_multichoice_helper.registerFrameWindowEventHandlers(e.detail.frameId);
+    }, true);
+
     console.log("omero_multichoice_helper js helper initialized!!!");
 };
 
+
+M.omero_multichoice_helper.registerFrameWindowEventHandlers = function(frameId){
+
+    var omero_viewer_frame = document.getElementById(frameId);
+    if(!omero_viewer_frame){
+        throw EventException("Frame " + frameId + " not found!!!");
+    }
+
+    var frameWindow = omero_viewer_frame.contentWindow;
+    frameWindow.addEventListener("roiShapeSelected", M.omero_multichoice_helper.roiShapeSelected);
+    frameWindow.addEventListener("roiShapeDeselected", M.omero_multichoice_helper.roiShapeDeselected);
+}
+
+
+/**
+ * Handle the RoiShapeSelection Event
+ *
+ * @param info
+ */
+M.omero_multichoice_helper.roiShapeSelected = function(info){
+    console.log("Check message", info);
+}
+
+/**
+ * Handle the RoiShapeDeselection event
+ * @param info
+ */
+M.omero_multichoice_helper.roiShapeDeselected = function(info){
+    console.log("Check message", info);
+}
