33be50971d91340505068b6acb3d53e4a351b58d
Marco Enrico Piras <kikkomep@crs4.it>
2015-09-15 10:57:32 +0200

    Replaced default rendered

--- renderer.php
+++ renderer.php
@@ -37,33 +37,110 @@ defined('MOODLE_INTERNAL') || die();
 require_once($CFG->dirroot . '/question/type/multichoice/renderer.php');
 
 
-class qtype_omeromultichoice_single_renderer extends qtype_multichoice_single_renderer {
-//    public function formulation_and_controls(question_attempt $qa,
-//            question_display_options $options) {
-//
-//        $question = $qa->get_question();
-//
-//        $questiontext = $question->format_questiontext($qa);
-//        $placeholder = false;
-//        if (preg_match('/_____+/', $questiontext, $matches)) {
-//            $placeholder = $matches[0];
-//        }
-//        $input = '**subq controls go in here**';
-//
-//        if ($placeholder) {
-//            $questiontext = substr_replace($questiontext, $input,
-//                    strpos($questiontext, $placeholder), strlen($placeholder));
-//        }
-//
-//        $result = html_writer::tag('div', $questiontext, array('class' => 'qtext'));
-//
-//        /* if ($qa->get_state() == question_state::$invalid) {
-//            $result .= html_writer::nonempty_tag('div',
-//                    $question->get_validation_error(array('answer' => $currentanswer)),
-//                    array('class' => 'validationerror'));
-//        }*/
-//        return $result;
-//    }
+class qtype_omeromultichoice_single_renderer extends qtype_multichoice_single_renderer
+{
+
+    public function formulation_and_controls(question_attempt $qa,
+                                             question_display_options $options)
+    {
+
+
+        $question = $qa->get_question();
+
+        echo "OMERO: $question->omeroimageurl";
+
+        $response = $question->get_response($qa);
+
+        $inputname = $qa->get_qt_field_name('answer');
+        $inputattributes = array(
+            'type' => $this->get_input_type(),
+            'name' => $inputname,
+        );
+
+        if ($options->readonly) {
+            $inputattributes['disabled'] = 'disabled';
+        }
+
+        $radiobuttons = array();
+        $feedbackimg = array();
+        $feedback = array();
+        $classes = array();
+        foreach ($question->get_order($qa) as $value => $ansid) {
+            $ans = $question->answers[$ansid];
+            $inputattributes['name'] = $this->get_input_name($qa, $value);
+            $inputattributes['value'] = $this->get_input_value($value);
+            $inputattributes['id'] = $this->get_input_id($qa, $value);
+            $isselected = $question->is_choice_selected($response, $value);
+            if ($isselected) {
+                $inputattributes['checked'] = 'checked';
+            } else {
+                unset($inputattributes['checked']);
+            }
+            $hidden = '';
+            if (!$options->readonly && $this->get_input_type() == 'checkbox') {
+                $hidden = html_writer::empty_tag('input', array(
+                    'type' => 'hidden',
+                    'name' => $inputattributes['name'],
+                    'value' => 0,
+                ));
+            }
+            $radiobuttons[] = $hidden . html_writer::empty_tag('input', $inputattributes) .
+                html_writer::tag('label',
+                    $this->number_in_style($value, $question->answernumbering) .
+                    $question->make_html_inline($question->format_text(
+                        $ans->answer, $ans->answerformat,
+                        $qa, 'question', 'answer', $ansid)),
+                    array('for' => $inputattributes['id']));
+
+            // Param $options->suppresschoicefeedback is a hack specific to the
+            // oumultiresponse question type. It would be good to refactor to
+            // avoid refering to it here.
+            if ($options->feedback && empty($options->suppresschoicefeedback) &&
+                $isselected && trim($ans->feedback)
+            ) {
+                $feedback[] = html_writer::tag('div',
+                    $question->make_html_inline($question->format_text(
+                        $ans->feedback, $ans->feedbackformat,
+                        $qa, 'question', 'answerfeedback', $ansid)),
+                    array('class' => 'specificfeedback'));
+            } else {
+                $feedback[] = '';
+            }
+            $class = 'r' . ($value % 2);
+            if ($options->correctness && $isselected) {
+                $feedbackimg[] = $this->feedback_image($this->is_right($ans));
+                $class .= ' ' . $this->feedback_class($this->is_right($ans));
+            } else {
+                $feedbackimg[] = '';
+            }
+            $classes[] = $class;
+        }
+
+        $result = '';
+        $result .= html_writer::tag('div', $question->format_questiontext($qa),
+            array('class' => 'qtext'));
+
+        $result .= html_writer::start_tag('div', array('class' => 'ablock'));
+        $result .= html_writer::tag('div', $this->prompt(), array('class' => 'prompt'));
+
+        $result .= html_writer::start_tag('div', array('class' => 'answer'));
+        foreach ($radiobuttons as $key => $radio) {
+            $result .= html_writer::tag('div', $radio . ' ' . $feedbackimg[$key] . $feedback[$key],
+                    array('class' => $classes[$key])) . "\n";
+        }
+        $result .= html_writer::end_tag('div'); // Answer.
+
+        $result .= html_writer::end_tag('div'); // Ablock.
+
+        if ($qa->get_state() == question_state::$invalid) {
+            $result .= html_writer::nonempty_tag('div',
+                $question->get_validation_error($qa->get_last_qt_data()),
+                array('class' => 'validationerror'));
+        }
+
+        return $result;
+    }
+
 //
 //    public function specific_feedback(question_attempt $qa) {
 //        // TODO.
