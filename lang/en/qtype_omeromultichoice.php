e3555472ce07e93fd976a7300b68f60e0b34f059
Marco Enrico Piras <kikkomep@crs4.it>
2015-11-30 17:28:56 +0100

    Added two localized strings

--- lang/en/qtype_omeromultichoice.php
+++ lang/en/qtype_omeromultichoice.php
@@ -43,6 +43,9 @@ $string['add_roi_answer'] = 'Add a new ROI based answer';
 
 $string['answer_type'] = "Answer type";
 
+$string['roi_shape_inspector'] = "ROI Shape Inspector";
+$string['answer_classes'] = "Answer Classes";
+
 $string['qtype_0'] = "Plaintext";
 $string['qtype_1'] = "ROI answer";
 
