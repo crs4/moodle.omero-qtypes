c5b114c94c8a70588b11483c62f57caecf32f94b
Marco Enrico Piras <kikkomep@crs4.it>
2015-12-01 14:24:17 +0100

    Added a template listener of 'frameLoaded' and 'RoiLoaded' events

--- handler.js
+++ handler.js
@@ -1,5 +1,26 @@
+M.omero_multichoice_handler = {
+    init: function () {
 
-self.addEventListener('message', function(e) {
-    console.log("Handler is sending: " + e.data);
-    self.postMessage(e.data);
-}, false);
+        console.log($);
+        //alert("Initialized!");
+
+
+        // ToDO: change the frameLoaded event
+        document.addEventListener("frameLoaded", function (e) {
+            var frame_id = e.detail.frame_id;
+            var frame = document.getElementById(frame_id);
+            var omero_image_viewer_controller = frame.contentWindow.omero_repository_image_viewer_controller;
+            if (!omero_image_viewer_controller)
+                throw new EventException("omero_repository_image_viewer_controller not found!");
+
+            console.log("Controller loaded!!!", omero_image_viewer_controller);
+
+            omero_image_viewer_controller.getModel().addEventListener(M.omero_multichoice_handler);
+
+        }, true);
+    },
+
+    "onImageModelRoiLoaded": function () {
+        console.log("Handler: OK!!!");
+    }
+};
