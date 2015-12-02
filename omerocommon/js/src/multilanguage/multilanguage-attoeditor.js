2c2047d6f8415b809ad526e1b674decd1ef4c2bd
Marco Enrico Piras <kikkomep@crs4.it>
2015-12-02 22:51:12 +0100

    Added an initial version of multi language elements

---
+++ omerocommon/js/src/multilanguage/multilanguage-attoeditor.js
@@ -0,0 +1,23 @@
+/**
+ * Created by kikkomep on 12/2/15.
+ */
+
+define("qtype_omerocommon/multilanguage-attoeditor",
+    [
+        'jquery',
+        'qtype_omerocommon/multilanguage-element',
+        'qtype_omerocommon/multilanguage-attoeditor'
+    ],
+    function ($, Element, Editor) {
+        // Private functions.
+
+
+        // Public functions
+        return {
+            initialize: function (str) {
+
+                console.log("Initialized", this);
+            }
+        };
+    }
+);
---
+++ omerocommon/js/src/multilanguage/multilanguage-element.js
@@ -0,0 +1,18 @@
+/**
+ * Created by kikkomep on 12/2/15.
+ */
+
+define("qtype_omerocommon/multilanguage-element",
+    ['jquery', 'qtype_omerocommon/moodle-forms-utils'],
+    function ($) {
+        // Private functions.
+
+        // Public functions
+        return {
+            initialize: function (str) {
+
+                console.log("Initialized", this);
+            }
+        };
+    }
+);
