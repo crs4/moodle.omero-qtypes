2826d895e31acd13de921d7e9e97ed5941ac950f
Marco Enrico Piras <kikkomep@crs4.it>
2015-12-02 22:48:47 +0100

    Updated script to autoload custom JS files

--- omerocommon/js/modules.php
+++ omerocommon/js/modules.php
@@ -12,6 +12,12 @@ function init_js_modules()
 {
     global $CFG, $PAGE, $plugins;
 
+    // Prefix of JS modules
+    $module_prefix = "qtype_omerocommon";
+
+    // Array containing name of modules
+    $modules = array();
+
     // include the $plugins definition
     include $CFG->dirroot . "/question/type/omerocommon/jquery/plugins.php";
 
@@ -21,12 +27,45 @@ function init_js_modules()
     $PAGE->requires->jquery_plugin('ui-css');
 
     // includes all plugin requirements
-    foreach($plugins as $name => $module){
+    foreach ($plugins as $name => $module) {
 
         if ($CFG->debug) {
             $PAGE->requires->jquery_plugin($name, "qtype_omerocommon");
         } else {
             echo "TODO: set the JS production files!!!";
         }
+    }
+
+
+    // Detect whether use 'src' or 'dist' folder
+    if ($CFG->debug) {
+        $source_folder = $CFG->dirroot . "/question/type/omerocommon/js/src";
+    } else {
+        echo "TODO: set the JS production files!!!";
+        $source_folder = $CFG->dirroot . "/question/type/omerocommon/js/dist";
+    }
+
+
+
+    // Scan JS folder
+    foreach(scandir($source_folder) as $foldername){
+
+        chdir("$source_folder");
+        if(strcmp($foldername, ".")!==0
+            && strcmp($foldername, "..")!==0
+            && is_dir("$foldername")){
+            foreach(scandir($foldername) as $filename){
+                $fileinfo = pathinfo($filename);
+                if(isset($fileinfo['extension']) && $fileinfo['extension']==='js'){
+                    $PAGE->requires->js(new moodle_url("$CFG->wwwroot/question/type/omerocommon/js/src/$foldername/$filename"));
+                    array_push($modules, $fileinfo['filename']);
+                }
+            }
+        }
+    }
+
+    // Call initialization function
+    foreach($modules as $module){
+        $PAGE->requires->js_call_amd("$module_prefix/$module", "initialize");
     }
 }
