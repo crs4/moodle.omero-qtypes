0137e5080f564ab678a72c8f42276128bdf5919e
Marco Enrico Piras <kikkomep@crs4.it>
2015-12-03 16:18:35 +0100

    Added template of the 'answer-plaintext' class

---
+++ omeromultichoice/js/src/answer/answer-plaintext.js
@@ -0,0 +1,67 @@
+/**
+ * Created by kikkomep on 12/2/15.
+ */
+
+define("qtype_omerocommon/answer-plaintext",
+    [
+        'jquery',
+        'qtype_omerocommon/moodle-forms-utils',
+        'qtype_omerocommon/answer-base',
+        'qtype_omerocommon/multilanguage-element',
+        'qtype_omerocommon/multilanguage-attoeditor'
+    ],
+    function ($, Editor, FormUtils) {
+        // Private functions.
+
+
+        // Public functions
+        return {
+            initialize: function (str) {
+                console.log("Initialized", this);
+
+                // defines the basic package
+                M.qtypes = M.qtypes || {};
+
+                // defines the specific package of this module
+                M.qtypes.omerocommon = M.qtypes.omerocommon || {};
+
+
+                /**
+                 * Defines MoodleFormUtils class
+                 * @type {{}}
+                 */
+                M.qtypes.omerocommon.AnswerPlaintext = function (answer_list_container_id) {
+
+                    // the reference to this scope
+                    var me = this;
+
+                    // Call the parent constructor
+                    M.qtypes.omerocommon.AnswerBase.call(this, answer_list_container_id);
+
+
+                    /**
+                     * Builds the answer
+                     *
+                     * @private
+                     */
+                    me._build = function () {
+                        me._answer_container = $('<div class="fitem" id="' + me.answerContainerId + '"></div>');
+                        me._answer_list_container.append(me._answer_container);
+
+                        me._form_utils.appendElement(me._answer_container, "Text", "<textarea>xxx</textarea>");
+                        me._form_utils.appendElement(me._answer_container, "Grade", "<select ><option>1</option></select>");
+                        me._form_utils.appendElement(me._answer_container, "Feedback", "<textarea>xxx</textarea>");
+                    };
+                };
+
+
+                // inherit
+                M.qtypes.omerocommon.AnswerPlaintext.prototype = new M.qtypes.omerocommon.AnswerBase();
+
+                // correct the constructor
+                M.qtypes.omerocommon.AnswerPlaintext.prototype.constructor = M.qtypes.omerocommon.AnswerPlaintext;
+            }
+        };
+    }
+)
+;
