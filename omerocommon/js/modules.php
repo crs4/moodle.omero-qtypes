95d90cf7212172aa46ab6b4d8a88c1ee47031f71
Marco Enrico Piras <kikkomep@crs4.it>
2015-12-03 11:40:11 +0100

    Parametrization of the utility function to load JS dependencies

--- omerocommon/js/modules.php
+++ omerocommon/js/modules.php
@@ -8,12 +8,17 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-function init_js_modules()
+/**
+ * Loads and initializes the JS modules
+ *
+ * @param $qtype_package the name of the qtype package
+ */
+function init_js_modules($qtype_package)
 {
     global $CFG, $PAGE, $plugins;
 
     // Prefix of JS modules
-    $module_prefix = "qtype_omerocommon";
+    $module_prefix = "qtype_$qtype_package";
 
     // Array containing name of modules
     $modules = array();
@@ -36,28 +41,27 @@ function init_js_modules()
         }
     }
 
-
     // Detect whether use 'src' or 'dist' folder
     if ($CFG->debug) {
-        $source_folder = $CFG->dirroot . "/question/type/omerocommon/js/src";
+        $source_folder = $CFG->dirroot . "/question/type/$qtype_package/js/src";
     } else {
         echo "TODO: set the JS production files!!!";
-        $source_folder = $CFG->dirroot . "/question/type/omerocommon/js/dist";
+        $source_folder = $CFG->dirroot . "/question/type/$qtype_package/js/dist";
     }
 
 
-
     // Scan JS folder
-    foreach(scandir($source_folder) as $foldername){
+    foreach (scandir($source_folder) as $foldername) {
 
         chdir("$source_folder");
-        if(strcmp($foldername, ".")!==0
-            && strcmp($foldername, "..")!==0
-            && is_dir("$foldername")){
-            foreach(scandir($foldername) as $filename){
+        if (strcmp($foldername, ".") !== 0
+            && strcmp($foldername, "..") !== 0
+            && is_dir("$foldername")
+        ) {
+            foreach (scandir($foldername) as $filename) {
                 $fileinfo = pathinfo($filename);
-                if(isset($fileinfo['extension']) && $fileinfo['extension']==='js'){
-                    $PAGE->requires->js(new moodle_url("$CFG->wwwroot/question/type/omerocommon/js/src/$foldername/$filename"));
+                if (isset($fileinfo['extension']) && $fileinfo['extension'] === 'js') {
+                    $PAGE->requires->js(new moodle_url("$CFG->wwwroot/question/type/$qtype_package/js/src/$foldername/$filename"));
                     array_push($modules, $fileinfo['filename']);
                 }
             }
