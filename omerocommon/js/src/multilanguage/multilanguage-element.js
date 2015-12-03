9a4ee264e2d461d5739721b75be8171c048204fb
Marco Enrico Piras <kikkomep@crs4.it>
2015-12-03 15:12:16 +0100

    Updated the 'multilanguage-element' with an initial set of essential features

--- omerocommon/js/src/multilanguage/multilanguage-element.js
+++ omerocommon/js/src/multilanguage/multilanguage-element.js
@@ -18,17 +18,48 @@ define("qtype_omerocommon/multilanguage-element",
 
                 M.qtypes.omerocommon.MultilanguageElement = function (element_id) {
 
-                    // a reference to this scope
-                    var me = this;
+                    // registers the id of the container element
+                    this.element_id = element_id;
 
-                    //
-                    me.element_id = element_id;
+                    // initializes the map of localized strings
+                    this._locale_text_map = {};
 
                     // instance
-                    me._form_utils = new M.qtypes.omerocommon.MoodleFormUtils();
+                    this._form_utils = new M.qtypes.omerocommon.MoodleFormUtils();
+                };
 
-                    //
-                    //me._form_utils.initTextArea(element_id);
+
+                var prototype = M.qtypes.omerocommon.MultilanguageElement.prototype;
+                prototype.init = function (locale_text_map, current_language) {
+                    this._current_language = current_language;
+                    if (locale_text_map && !this._locale_text_map) {
+                        this._locale_text_map = locale_text_map;
+                    }
+                };
+
+                prototype.setLocaleText = function (text, language) {
+                    language = language || this._current_language;
+                    console.log("Setting locale string: ", language, text);
+                    this._locale_text_map[language] = text;
+                };
+
+                prototype.getLocaleText = function (language) {
+                    language = language || this._current_language;
+                    return this._locale_text_map[language];
+                };
+
+                prototype.changeLanguage = function (language) {
+                    this.save();
+                    this._current_language = language;
+                    console.info("Changing language to: " + language);
+                };
+
+                prototype.onLanguageChanged = function (language) {
+                    this.changeLanguage(language);
+                };
+
+                prototype.clear = function () {
+                    console.warn("Not implemented at this level");
                 }
             }
         };
