86e852f9384300883e29438dfd45cfb8cf842c8b
Marco Enrico Piras <kikkomep@crs4.it>
2015-12-03 16:17:29 +0100

    Added question-base class (initial version)

---
+++ omerocommon/js/src/question/question-base.js
@@ -0,0 +1,50 @@
+/**
+ * Created by kikkomep on 12/3/15.
+ */
+/**
+ * Created by kikkomep on 12/2/15.
+ */
+
+define("qtype_omerocommon/question-base",
+    [
+        'jquery',
+        'qtype_omerocommon/moodle-forms-utils',
+        'qtype_omerocommon/answer-base',
+        'qtype_omerocommon/multilanguage-element',
+        'qtype_omerocommon/multilanguage-attoeditor'
+    ],
+    function ($, Editor, FormUtils) {
+        // Private functions.
+
+
+        // Public functions
+        return {
+            initialize: function (str) {
+                console.log("Initialized", this);
+
+                // defines the basic package
+                M.qtypes = M.qtypes || {};
+
+                // defines the specific package of this module
+                M.qtypes.omerocommon = M.qtypes.omerocommon || {};
+
+
+                /**
+                 * Builds a new instance
+                 *
+                 * @constructor
+                 */
+                M.qtypes.omerocommon.QuestionBase = function () {
+
+                    // the reference to this scope
+                    var me = this;
+                };
+
+
+                // A local reference to the prototype
+                var prototype = M.qtypes.omerocommon.QuestionBase.prototype;
+
+            }
+        };
+    }
+);
