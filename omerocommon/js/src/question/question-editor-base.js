fc3e0a1a0d740ba04921195bbb00a40ea1ffaa93
Marco Enrico Piras <kikkomep@crs4.it>
2015-12-04 07:46:49 +0100

    Restored support for a common set of multi language fields

--- omerocommon/js/src/question/question-editor-base.js
+++ omerocommon/js/src/question/question-editor-base.js
@@ -63,12 +63,81 @@ define("qtype_omerocommon/question-editor-base",
 
                     // the reference to this scope
                     var me = this;
+
+                    me._localized_string_names = [
+                        "questiontext",
+                        "generalfeedback",
+                        "correctfeedback", "partiallycorrectfeedback", "incorrectfeedback"
+                    ];
                 };
 
 
                 // A local reference to the prototype
                 var prototype = M.qtypes.omerocommon.QuestionEditorBase.prototype;
 
+
+                /**
+                 * Initializes this questionEditor controller
+                 */
+                prototype.initialize = function () {
+                    var me = this;
+                    me._editor = {};
+                    for (var i in me._localized_string_names) {
+                        var localized_string_name = me._localized_string_names[i];
+                        var editor = new M.qtypes.omerocommon.MultilanguageAttoEditor("id_" + localized_string_name, true);
+                        editor.init({}, "en");
+                        // registers a reference to the editor instance
+                        me._editor[localized_string_name] = editor;
+                    }
+
+
+                    // registers the editor as listener of the 'LanguageChanged' event
+                    language_selector.on("change",
+                        function (event) {
+                            me.onLanguageChanged($(event.target).val());
+                        }
+                    );
+
+                    // TODO: initialize me!!!!
+                    var frame_id = "omero-image-viewer";
+                    var visible_roi_list = [];
+
+                    // register the frame when loaded
+                    document.addEventListener("frameLoaded", function (e) {
+                        me.onViewerFrameLoaded(e.detail.frame_id, visible_roi_list, e.detail);
+                    }, true);
+                };
+
+
+                /**
+                 * Returns the list of supported languages
+                 *
+                 * @returns {*|Array.<T>|string|Blob|ArrayBuffer}
+                 */
+                prototype.getSupportedLanguages = function () {
+                    return _supported_languages.slice();
+                };
+
+
+                /**
+                 * Return the current selected language
+                 * @returns {*}
+                 */
+                prototype.getCurrentLanguage = function () {
+                    return language_selector.val();
+                };
+
+
+                /**
+                 * Handler for ths 'LanguageChanged' event:
+                 * it updates the text of all editors
+                 * @param language
+                 */
+                prototype.onLanguageChanged = function (language) {
+                    for (var locale_string in this._editor) {
+                        this._editor[locale_string].onLanguageChanged(language);
+                    }
+                };
             }
         };
     }
