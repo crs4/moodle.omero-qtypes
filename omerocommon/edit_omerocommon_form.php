8b52f4d9562d6cd46ec75da816e5957af772a680
Marco Enrico Piras <kikkomep@crs4.it>
2015-12-10 00:35:46 +0100

    Updated base question-form to include the controls to update the image properties

--- omerocommon/edit_omerocommon_form.php
+++ omerocommon/edit_omerocommon_form.php
@@ -267,14 +267,32 @@ abstract class qtype_omerocommon_edit_form extends qtype_multichoice_edit_form
                 'omero_image_server' => get_config('omero', 'omero_restendpoint'))
         );
 
-        $mform->addElement('html', '<div id="omero-image-viewer-toolbar" class="hidden">');
         $mform->addElement('html', '
-            <div class="checkbox disabled">
-              <label for="omero-image-view-lock">
-                Lock the view
-              </label>
-              <input id="omero-image-view-lock" name="omero-image-view-lock" type="checkbox"data-toggle="toggle">
-            </div>
+
+                        <div id="omero-image-viewer-toolbar" class="hidden">
+                        <div class="checkboxx">
+                          <div style="display: inline-block;">
+                          <a id="omero-image-properties-update" href="javascript:void(0)" title="Update image center">
+                            <i class="glyphicon glyphicon-screenshot"></i>
+                          </a>
+                          <span id="omero-image-viewer-properties">x: 123123, y: 12312312, zm: 123123123</span>
+                          </div>
+                          <div id="omero-image-view-lock-container">
+                              <label for="omero-image-view-lock">
+                                image navigation:
+                              </label>
+                              <input id="omero-image-view-lock" name="omero-image-view-lock" data-toggle="toggle"
+                                     type="checkbox" data-onstyle="success" data-offstyle="danger">
+                          </div>
+                        </div>
+
+
+            <script>
+                $(\'#omero-image-view-lock\').bootstrapToggle($(\'[name^=omeroimagelocked]\').val()=="1" ? "on" : "off");
+                $(\'#omero-image-view-lock\').change(function(){
+                    M.qtypes.omerocommon.QuestionEditorBase.getInstance().onLockImageChanged($(this).is(\':checked\'));
+                });
+            </script>
         ');
         $mform->addElement('html', '</div>');
 
@@ -732,7 +750,5 @@ abstract class qtype_omerocommon_edit_form extends qtype_multichoice_edit_form
 
             return $data;
         }
-
-
     }
 }
