7ce214edfda9b34df6ecb2f339161899eac1dcfb
Marco Enrico Piras <kikkomep@crs4.it>
2015-12-09 08:27:06 +0100

    Added function to mix ROI shape properties

--- omerocommon/js/src/question/roi-shape-model.js
+++ omerocommon/js/src/question/roi-shape-model.js
@@ -27,12 +27,26 @@ define("qtype_omerocommon/roi-shape-model",
                 M.qtypes.omerocommon = M.qtypes.omerocommon || {};
 
                 // constructor
-                M.qtypes.omerocommon.RoiShapeModel = function(raw_roi_shape, visible){
-                    for(var i in raw_roi_shape){
+                M.qtypes.omerocommon.RoiShapeModel = function (raw_roi_shape, visible) {
+                    for (var i in raw_roi_shape) {
                         this[i] = raw_roi_shape[i];
                     }
-                    this["visible"] = visible;
-                }
+                    this.visible = visible;
+                };
+
+                // shortcut for the 'class'
+                var theClass = M.qtypes.omerocommon.RoiShapeModel;
+
+                //
+                theClass.toRoiShapeModel = function (roi_shape_list, visible_roi_list) {
+                    var result = [];
+                    for (var i in roi_shape_list) {
+                        var roi = roi_shape_list[i];
+                        console.log("CHECK visibility", roi.id, roi, visible_roi_list.indexOf(roi.id) !== -1);
+                        result.push(new theClass(roi, visible_roi_list.indexOf(roi.id) !== -1));
+                    }
+                    return result;
+                };
             }
         };
     }
