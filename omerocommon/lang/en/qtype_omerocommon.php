6fc414cfea6b4d0dee2eae76d4aba70d47964915
Marco Enrico Piras <kikkomep@crs4.it>
2015-12-10 00:42:57 +0100

    Updated localized strings

--- omerocommon/lang/en/qtype_omerocommon.php
+++ omerocommon/lang/en/qtype_omerocommon.php
@@ -27,8 +27,8 @@ $string['pluginname'] = 'Omero MultiChoice';
 $string['pluginname_help'] = 'Create a cloze question type with embedded response fields in the question text to enter a numeric
 or text value or select a value from a number of options.';
 $string['pluginname_link'] = 'question/type/omeromultichoice';
-$string['pluginnameadding'] = 'Adding a Omero MultiChoice question';
-$string['pluginnameediting'] = 'Editing a Omero MultiChoice question';
+$string['pluginnameadding'] = 'Adding a Omero MultiChoice Question';
+$string['pluginnameediting'] = 'Editing a Omero MultiChoice Question';
 $string['pluginnamesummary'] = 'A Omero MultiChoice question type which allows the embedding of the response fields for various available
 sub questions in the question text.
 So the student can enter a numeric or short text answer or choose an answer or answer(s) from
