d9fd756a6fbcbea2648448c393ed34b27a0b2b4e
Marco Enrico Piras <kikkomep@crs4.it>
2015-12-17 12:37:19 +0100

    Code cleaning

--- omerointeractive/js/src/main/main.js
+++ omerointeractive/js/src/main/main.js
@@ -12,10 +12,11 @@ define("qtype_omerointeractive/main",
         'qtype_omerocommon/question-editor-base',
         'qtype_omerointeractive/question-editor-interactive'
     ],
-    function ($, a, b, c, d, e, f) {
+    function (j, a, b, c, d, e, f) {
 
         // Private functions.
         // ...
+        var $ = jQuery;
 
         // Public functions
         return {
@@ -31,12 +32,36 @@ define("qtype_omerointeractive/main",
                     // defines the specific package of this module
                     M.qtypes.omerointeractive = M.qtypes.omerointeractive || {};
 
-                    var common = M.qtypes.omerocommon;
-                    var interactive = M.qtypes.omerointeractive;
-
-                    var question_editor = new interactive.QuestionEditorInteractive();
-                    question_editor.initialize();
+                    //var common = M.qtypes.omerocommon;
+                    //var interactive = M.qtypes.omerointeractive;
+                    //
+                    //var question_editor = new interactive.QuestionEditorInteractive();
+                    //question_editor.initialize();
+                    //
+                    //
+                    //// TODO: remove me!!!!!
+                    //var mgt = new ImageModelManager("http://omero-test.crs4.it:8080", "1");
+                    //mgt.addEventListener(question_editor);
+                    //mgt.loadRoisInfo(function(){
+                    //    console.log("Loaded ROIs");
+                    //});
+                    //
+                    //
+                    ////TODO: remove me... just to debug
+                    //window.theTable = question_editor._roi_shape_table;
+                    //window.question_editor = question_editor;
+                    //window.model_manager = mgt;
                 });
+            },
+
+
+            test: function(question){
+                console.log($("#omero-image-view-lock").bootstrapToggle(), $(document));
+                alert("TEST");
+
+                $("#omero-image-view-lock").on("change", function(){
+                    console.log("Changed!!!");
+                })
             }
         };
     }
