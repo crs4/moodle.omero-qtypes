46472c9fe4ec294b1166e01925b956da56b0e8bc
Marco Enrico Piras <kikkomep@crs4.it>
2016-01-18 15:52:00 +0100

    Fixed center controller visualization

--- omerointeractive/renderer.php
+++ omerointeractive/renderer.php
@@ -354,14 +354,15 @@ abstract class qtype_omerointeractive_base_renderer extends qtype_multichoice_re
 
 
         $image_properties = null;
-        if ($question->omeroimageproperties) {
-            $image_properties = json_decode($question->omeroimageproperties);
-            $result .= '<div class="panel image_position_button">' .
-                '<span class="pull-right sm">' .
+        $image_properties = json_decode($question->omeroimageproperties);
+        $result .= '<div class="panel image_position_button">' .
+            '<span class="pull-right sm">' .
+            (($question->omeroimageproperties) ?
                 '(x,y): ' . $image_properties->center->x . ", " . $image_properties->center->y .
-                '<i class="restore-image-center-btn pull-right glyphicon glyphicon-screenshot" style="margin-left: 10px;">' .
-                '</i></span></div>';
-        }
+                '<i class="restore-image-center-btn pull-right glyphicon glyphicon-screenshot" style="margin-left: 10px;"></i>' :
+                ""
+            ) .
+            '</span></div>';
 
         $result .= '<div id="' . self::MARKER_REMOVERS_CONTAINER . '" ' .
             ' class="panel remove_marker_button_group">' .
