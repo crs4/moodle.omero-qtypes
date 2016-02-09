82e2d22dc4ed83fcccd67421767ee7e1722281d5
Marco Enrico Piras <kikkomep@crs4.it>
2016-02-09 12:36:11 +0100

    Refactored the 'omerocommon' locale file

--- omerocommon/lang/en/qtype_omerocommon.php
+++ omerocommon/lang/en/qtype_omerocommon.php
@@ -23,74 +23,58 @@
 
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-$string['pluginname'] = 'Omero MultiChoice';
-$string['pluginname_help'] = 'Create a cloze question type with embedded response fields in the question text to enter a numeric
-or text value or select a value from a number of options.';
-$string['pluginname_link'] = 'question/type/omeromultichoice';
-$string['pluginnameadding'] = 'Adding a Omero MultiChoice Question';
-$string['pluginnameediting'] = 'Editing a Omero MultiChoice Question';
-$string['pluginnamesummary'] = 'A Omero MultiChoice question type which allows the embedding of the response fields for various available
-sub questions in the question text.
-So the student can enter a numeric or short text answer or choose an answer or answer(s) from
- using a select box, check boxes or radio boxes.';
 
+# generics
+$string['pluginname'] = 'Omero Question Common Library';
+$string['pluginname_help'] = 'Common library shared between Omero Question types';
+$string['pluginname_link'] = 'question/type/omerocommon';
+$string['pluginnamesummary'] = 'Common library shared between Omero Question types.';
+
+# language settings
+$string['language'] = "Language";
+
+# classifiers (i.e., question tags)
 $string['questionclassifiers'] = 'Classifiers';
 $string['selectquestionclassifiers'] = 'Select one or more question classifiers';
 $string['editquestionclassifiers'] = 'Edit';
 
-
+# answers
+$string['answer_options_properties'] = 'Properties of answer options';
+$string['add_answers'] = 'Add answers';
+$string['choiceno'] = '{$a}';
 $string['answer_choiceno'] = "Answer choice n. ";
 $string['answer_grade'] = "Grade";
+$string['answerhowmany'] = 'One or multiple correct answers?';
+$string['answersingleno'] = 'Multiple correct answers allowed';
+$string['answersingleyes'] = 'One correct answer only';
 
-$string['answer_options_properties'] = 'Properties of answer options';
-
+# feedback
 $string['general_and_combined_feedback'] = 'General and combined feedback';
 
-$string['add_roi_answer'] = 'Add new ROI answer';
-
-$string['choiceno'] = '{$a}';
-
-$string['roi_choiceno'] = 'ROI choice {$a}';
-
-$string['add_roi_answer'] = 'Add a new ROI based answer';
-
-$string['answer_type'] = "Answer type";
-
-$string['roi_shape_inspector'] = "ROI Shapes Inspector";
-
+# image viewer
 $string['omero_image_viewer'] = "Omero Image Viewer";
-$string['omero_image_and_rois'] = "Omero Image and ROIs";
 $string['image_viewer_lock_student_navigation'] = "lock student navigation";
 
-$string['qtype_0'] = "Plaintext";
-$string['qtype_1'] = "ROI answer";
+# ROIs
+$string['roi_shape_inspector'] = "ROI Shape Inspector";
+$string['roi_id'] = 'Identifier';
+$string['roi_comment'] = 'Comment';
+$string['roi_type'] = 'Type';
+$string['roi_width'] = 'Width';
+$string['roi_height'] = 'Height';
+$string['roi_shape_details'] = "ROI Shape Details";
+$string['roi_description'] = "Description";
+$string['roi_visibility'] = "Visibility";
+$string['roi_focus'] = "Focus";
 
-
+# save/update controls
 $string['savechangesandcontinueediting'] = "Save";
 $string['savechangesandexit'] = "Save and Exit";
 
+# validation messages
 $string['validate_warning'] = "Warning";
 $string['validate_no_answers'] = "Answers are less than 1 !!!";
 $string['validate_no_image'] = "No image selected !!!";
 $string['validate_at_least_one_100'] = "One of the choices should have grade 100% !!!";
 $string['validate_at_most_one_100'] = "At most one answer should have grade 100% !!!";
 $string['validate_sum_of_grades'] = "The sum of grades should be equal to 100% !!!";
-
-
-$string['roi_id'] = 'Identifier';
-$string['roi_comment'] = 'Comment';
-$string['roi_type'] = 'Type';
-$string['roi_width'] = 'Width';
-$string['roi_height'] = 'Height';
-
-$string['roi_shape_details'] = "ROI Shape Details";
-$string['roi_description'] = "Description";
-$string['roi_visibility'] = "Visibility";
-$string['roi_focus'] = "Focus";
-
-
-$string['language'] = "Language";
-
-$string['answerhowmany'] = 'One or multiple correct answers?';
-$string['answersingleno'] = 'Multiple correct answers allowed';
-$string['answersingleyes'] = 'One correct answer only';
