d33d978cf7919e9b262b5528fed5bebc0162c6fc
Marco Enrico Piras <kikkomep@crs4.it>
2016-02-09 15:33:02 +0100

    Added locale it of the omeromultichoice module

---
+++ omeromultichoice/lang/it/qtype_omeromultichoice.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_omeromultichoice', language 'en', branch 'MOODLE_29_STABLE'
+ *
+ * @package    qtype
+ * @subpackage omeromultichoice
+ * @copyright  2015 CRS4
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+# generics
+$string['pluginname'] = 'Omero MultiChoice';
+$string['pluginname_link'] = 'question/type/omeromultichoice';
+$string['pluginnameadding'] = 'Inserisci una domanda "Omero MultiChoice"';
+$string['pluginnameediting'] = 'Modifica una domanda "Omero MultiChoice"';
+$string['pluginname_help'] = 'Tipo di domanda a risposta multipla che consente la scelta e l\'associazione di un\'immagine Omero e la selezione di un insieme di ROI (Region Of Interest) da visualizzare.';
+$string['pluginnamesummary'] = 'Tipo di domanda a risposta multipla che consente la scelta e l\'associazione di un\'immagine Omero e la selezione di un insieme di ROI (Region Of Interest) da visualizzare..';
+
+# answers
+$string['add_answers'] = 'Aggiungi risposte';
+
+# feedback
+$string['correctansweris'] = 'La sola risposta corretta è: ';
+$string['correctanswerare'] = 'Le risposte corrette sono: ';
