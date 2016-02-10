5759baf7244680cc6030af5afd4bff40be9ede3e
Marco Enrico Piras <kikkomep@crs4.it>
2016-02-10 12:36:30 +0100

    Updated copyright and licence of the 'qtype_omerocommon' module

--- omerocommon/js/src/html-utils.js
+++ omerocommon/js/src/html-utils.js
@@ -1,3 +1,22 @@
+// Copyright (c) 2015-2016, CRS4
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 /** Utility instance */
 M.omero_multichoice_html_utils = {};
 
@@ -8,26 +27,7 @@ var me = M.omero_multichoice_html_utils;
 /**
  * Initializes this module
  */
-me.init = function () {
-
-    //$('[data-toggle="popover"]').popover();
-    //
-    //
-    //$("#enableModal").click(function () {
-    //    $('#myModal').modal();
-    //    //$('#myInput').focus();
-    //});
-    //
-    //
-    //$(".dropdown-toggle").dropdown();
-    //
-    //$('#username').editable({
-    //
-    //    success: function (response, newValue) {
-    //        alert("Changed: !!!");
-    //    }
-    //});
-};
+me.init = function () {};
 
 /**
  * Initializes a WYSIWYG editor bounded to a given textarea
