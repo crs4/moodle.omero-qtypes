bf519858761de3cda178f2235fb5d1f319b49698
Marco Enrico Piras <kikkomep@crs4.it>
2016-04-19 11:38:02 +0200

    add script to register the 'omerquestiontags' form

---
+++ register.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ -z ${MOODLE_WWW} ]]; then
+	echo -e "ERROR: MOODLE_WWW not found in your environment !!!"
+	exit -1
+fi
+
+if grep -o 'MoodleQuickForm_omeroquestiontags' ${MOODLE_WWW}/lib/formslib.php ; then
+	echo -e "\n NOTICE: 'omeroquestiontags' form already registered."
+else	
+	echo -e "# OmeroQuestionTags \nMoodleQuickForm::registerElementType('omeroquestiontags', \"\$CFG->dirroot/question/type/omerocommon/omeroquestiontags.php\", 'MoodleQuickForm_omeroquestiontags');" >> ${MOODLE_WWW}/lib/formslib.php
+	echo -e "Registering the 'omeroquestiontags' form.... done."
+fi
+
