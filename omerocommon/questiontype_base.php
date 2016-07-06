8975899e8065e823b5bb293f566fa86cbdd09d69
Marco Enrico Piras <kikkomep@crs4.it>
2016-07-06 17:14:54 +0200

    Renamed to 'div' the HTML element acting as locale text container

--- omerocommon/questiontype_base.php
+++ omerocommon/questiontype_base.php
@@ -145,7 +145,7 @@ abstract class qtype_omerocommon extends qtype_multichoice
         $json_data = json_decode($json_format);
         foreach ($json_data as $lang => $text) {
             if (!empty(strip_tags($text)) && !in_array($lang, $languages)) {
-                $result .= '<span class="multilang" lang="' . $lang . '">' . $text . '</span>';
+                $result .= '<div class="multilang" lang="' . $lang . '">' . $text . '</span>';
                 array_push($languages, $lang);
             }
         }
