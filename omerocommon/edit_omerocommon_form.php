ab58533a5c540368e05f7129b903ad2d5c6bc2ac
Marco Enrico Piras <kikkomep@crs4.it>
2016-08-02 11:53:58 +0200

    Fixed form initialization with the feedback images stored by the Moodle persistence layer

--- omerocommon/edit_omerocommon_form.php
+++ omerocommon/edit_omerocommon_form.php
@@ -810,7 +810,7 @@ abstract class qtype_omerocommon_edit_form extends qtype_multichoice_edit_form
             $question->feedbackformat[$key] = $answer->feedbackformat;
 
             // TODO: update with real data: remove the empty array !!!
-            $question->feedbackimages[$key] = json_encode(array());
+            $question->feedbackimages[$key] = empty($answer->images) ? json_encode(array()) : $answer->images;
 
             $question->answer_locale_map[$key] = json_encode(qtype_omerocommon::serialize_to_json_from($answer->answer));
             $question->feedback_locale_map[$key] = json_encode(qtype_omerocommon::serialize_to_json_from($answer->feedback));
