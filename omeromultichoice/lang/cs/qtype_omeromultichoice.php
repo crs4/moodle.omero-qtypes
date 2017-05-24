926f7d36a4b4956a1347709f83a886f13e9b0579
Marco Enrico Piras <kikkomep@crs4.it>
2017-05-24 18:29:00 +0200

    Add CS translation of the 'omeromultichoice' module

---
+++ omeromultichoice/lang/cs/qtype_omeromultichoice.php
@@ -0,0 +1,47 @@
+<?php
+
+// Copyright (c) 2015-2016, CRS4
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+/**
+ * Strings for the module 'qtype_omeromultichoice', language 'cs'
+ *
+ * @package    qtype
+ * @subpackage omeromultichoice
+ * @copyright  2015-2016 CRS4
+ * @license    https://opensource.org/licenses/mit-license.php MIT license
+ */
+
+# standardy
+$string['pluginname'] = 'Omero MultiChoice';
+$string['pluginname_link'] = 'Otázka/typ/omeromultichoice';
+$string['pluginnameadding'] = 'Pøidání"Omero MultiChoice" otázky';
+$string['pluginnameediting'] = 'Editace "Omero MultiChoice" otázky';
+$string['pluginname_help'] = 'Otázka s výbìrem možností, která dovoluje vybrat Omero obrázek a zvolit set ROI (Region Of Interest - oblastí zájmu) definovaných ve zobrazovaném obrázku.';
+$string['pluginnamesummary'] = 'Otázka s výbìrem možností, která dovoluje vybrat Omero obrázek a zvolit set ROI (Region Of Interest - oblastí zájmu) definovaných ve zobrazovaném obrázku.';
+
+# answers
+$string['add_answers'] = 'Pøidej odpovìdi';
+$string['answer_text'] = 'Text';
+
+# feedback
+$string['correctanswersno'] = "Poèet správných odpovìdí: ";
+$string['correctansweris'] = 'Jediná správná odpovìï je: ';
+$string['correctandcompleteansweris'] = 'Správná a úplná odpovìï je: ';
+$string['correctanswerare'] = 'Správné odpovìdi jsou pouze: ';
